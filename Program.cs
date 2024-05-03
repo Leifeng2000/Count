31e67b8626119a772f030729e1bd10510e2beca8
Leifeng2000 <leifeng7112@gmail.com>
2024-05-03 16:32:14 +0700

    Up

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = "Hello, World!";
+        
+        Console.Write("Enter a character: ");
+        char character = Console.ReadLine()[0];
+
+        int count = 0;
+        
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == character)
+            {
+                count++;
+            }
+        }
+        
+        Console.WriteLine($"Number of occurrences of '{character}' in the string: {count}");
+    }
+}
